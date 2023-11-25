5d204947b127b5e7eda3dbacbba7a8c0a3432d37
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2023-11-25 14:00:23 +0530

    update

--- force-app/main/default/lwc/childlwc/childlwc.js
+++ force-app/main/default/lwc/childlwc/childlwc.js
@@ -3,14 +3,25 @@
  * @last modified on  : 25-11-2023
  * @last modified by  : Vrushabh Uprikar
 **/
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import temp1 from './childlwc1.html';
 import temp2 from './childlwc2.html';
 export default class Childlwc extends LightningElement {
 
     @api handleSwitch;
+    @track any;
+    value;
 
     render() {
-        return handleSwitch ? temp1 : temp2;
+        console.log('render:');
+        return this.handleSwitch ? temp1 : temp2;
+    }
+    renderedCallback() {
+        if (this.handleSwitch) {
+            this.any = 'Test ' + this.handleSwitch;
+            this.value = 'When It is True';
+        } else {
+            this.value = 'When It is False';
+        }
     }
 }
